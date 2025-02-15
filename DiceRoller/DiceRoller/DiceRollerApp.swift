15e6180c7e6e798ef29a94aeceae6a4373d67603
YeEum Lee <yeeumm@gmail.com>
2025-02-15 01:35:10 -0800

    DIceRoller - learn State

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by YeEum Lee on 2025-02-14.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceRoller/DiceRoller/DiceView.swift
@@ -0,0 +1,34 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by YeEum Lee on 2025-02-14.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    @State private var numberOfPips: Int = 1
+    
+    var body: some View {
+        
+        VStack {
+            Image(systemName: "die.face.\(numberOfPips).fill")
+                .resizable()
+                .frame(maxWidth: 100, maxHeight: 100)
+                .aspectRatio(1, contentMode: .fit)
+                .foregroundStyle(.black, .white)
+            
+            Button("Roll") {
+                withAnimation {
+                    numberOfPips = Int.random(in: 1...6)
+                }
+            }.buttonStyle(.bordered)
+        }
+
+    }
+}
+
+#Preview {
+    DiceView()
+}
